ec5f0b0458fe2e426100590723f0f73242ecdef6
shubham <shubham.bhoyar077@gmail.com>
2023-02-28 15:20:58 +0530

    Added test for checkoing throw errors

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,10 @@
 function stringLength(str){
-  return str.length;
+  if(str.length > 0 && str.length <= 10){
+    return str.length;
+  }
+  else{
+    throw new Error("string length should be in between 1 and 10");
+  }
 }
 
 module.exports = stringLength;
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,5 +1,9 @@
 const stringLength = require('./stringLength');
 
 test("String Length", () => {
-  expect(stringLength("shubham")).toBe(7);
+  expect(stringLength("example")).toBe(7);
+});
+
+test("Check string length", () => {
+  expect(() => stringLength("example1234567")).toThrow('string length should be in between 1 and 10');
 });
